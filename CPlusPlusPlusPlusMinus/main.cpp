1282acfa3367db9eb8fd641ed07c0a9e036d8c99
inperium <ibow9416@740s-pl-111.local>
2017-01-23 12:34:37 -0700

    CPlusPlusPlusPlusPlusPlusMinus

---
+++ CPlusPlusPlusPlusMinus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPlusPlusPlusPlusMinus
+//
+//  Created by Bowen, Isaac on 1/23/17.
+//  Copyright © 2017 Bowen, Isaac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
